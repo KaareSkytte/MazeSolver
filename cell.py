63fe4276b3e157e507035cfb9a8cd9236912f0b6
KaareSkytte <kaareskytte1431@gmail.com>
2025-03-23 09:21:06 +0100

    Added Solve function

--- cell.py
+++ cell.py
@@ -1,27 +1,26 @@
 from graphics import Line, Point
 
+
 class Cell:
     def __init__(self, win=None):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
+        self.visited = False
         self._x1 = None
         self._x2 = None
         self._y1 = None
         self._y2 = None
         self._win = win
-        self.visited = False
 
     def draw(self, x1, y1, x2, y2):
         if self._win is None:
             return
-        
         self._x1 = x1
         self._x2 = x2
         self._y1 = y1
         self._y2 = y2
-
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
             self._win.draw_line(line)
--- graphics.py
+++ graphics.py
@@ -1,37 +1,30 @@
 from tkinter import Tk, BOTH, Canvas
 
+
 class Window:
     def __init__(self, width, height):
-        self.height = height
-        self.width = width
         self.__root = Tk()
         self.__root.title("Maze Solver")
-        
-        self.__canvas = Canvas(self.__root, bg="white")
-        self.__canvas.pack(fill=BOTH, expand = 1)
-        
-        self.running = False
-
         self.__root.protocol("WM_DELETE_WINDOW", self.close)
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__running = False
 
     def redraw(self):
         self.__root.update_idletasks()
         self.__root.update()
 
-    
     def wait_for_close(self):
-        self.running = True
-        while self.running == True:
+        self.__running = True
+        while self.__running:
             self.redraw()
         print("window closed...")
 
-
     def draw_line(self, line, fill_color="black"):
-        line.draw(self.__canvas, fill_color)    
+        line.draw(self.__canvas, fill_color)
 
     def close(self):
-        self.running = False
-
+        self.__running = False
 
 
 class Point:
@@ -40,11 +33,16 @@ class Point:
         self.y = y
 
 
-
 class Line:
-    def __init__(self, p1, p2):
+    def __init__(
+        self,
+        p1,
+        p2,
+    ):
         self.p1 = p1
         self.p2 = p2
 
     def draw(self, canvas, fill_color="black"):
-        canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2)
+        canvas.create_line(
+            self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fill_color, width=2
+        )
